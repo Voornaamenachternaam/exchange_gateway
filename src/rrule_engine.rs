e25e3bbf4fb98caa2ab86214f6d20af68c9c40a2
Voornaamenachternaam <84537103+Voornaamenachternaam@users.noreply.github.com>
2026-02-21 19:51:43 +0100

    Update rrule_engine.rs

--- src/rrule_engine.rs
+++ src/rrule_engine.rs
@@ -1,11 +1,18 @@
-// rrule_engine.rs
 use anyhow::Result;
 use chrono::{DateTime, Utc};
 use rrule::RRule;
 
-pub fn expand_rrule(dtstart: DateTime<Utc>, rrule_str: &str, start: DateTime<Utc>, end: DateTime<Utc>) -> Result<Vec<DateTime<Utc>>> {
+/// Expand an RFC 5545 RRULE between two dates.
+pub fn expand_rrule(
+    dtstart: DateTime<Utc>,
+    rrule_str: &str,
+    start: DateTime<Utc>,
+    end: DateTime<Utc>,
+) -> Result<Vec<DateTime<Utc>>> {
     let rule = rrule_str.parse::<RRule>()?;
     let all = rule.all(chrono::Utc, Some(dtstart), Some(end))?;
-    let res: Vec<DateTime<Utc>> = all.into_iter().filter(|d| *d >= start && *d <= end).collect();
+    let res: Vec<DateTime<Utc>> = all.into_iter()
+        .filter(|d| *d >= start && *d <= end)
+        .collect();
     Ok(res)
 }
